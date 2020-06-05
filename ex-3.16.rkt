fd2491b66f17b69e541c15a8c3eba3d52a57f4a2
Caio Lins <caiolinspeixoto@gmail.com>
2020-06-05 11:17:47 -0300

    add resto do ex-3.16

--- ex-3.16.rkt
+++ ex-3.16.rkt
@@ -21,9 +21,18 @@ lst1
 lst2
 
 ;; return 7
+
 (define w (cons 3 nil))
 (define y (cons w w))
 (define lst3 (cons y y))
 (count-pairs lst3)
 lst3
+
+;; does not return
+
+(define lst4 (cons 1 3))
+(define b (cons 3 lst4))
+(define a (cons 2 b))
+(set-car! lst4 a)
+(count-pairs lst4)
 
